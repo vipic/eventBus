66299b65a8751479eab5067a4c46faa85a302607
Mason <read_xms@hotmail.com>
2020-08-06 22:26:32 +0800

    完善

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 class EventBus {
-  cache = {};
-  on(eventName, fn) {
+  constructor() {
+    this.cache = {}
+  }
+
+  on(eventName, fn, ctx = null) {
+    if (typeof fn !== 'function') return console.log('arguments[1] must be a function')
+
     this.cache[eventName] = this.cache[eventName] || [];
-    this.cache[eventName].push(fn);
+
+    if (!this.cache[eventName].find(item => item.fn === fn)) {
+      this.cache[eventName].push({ctx, fn});
+    }
   }
-  emit(eventName) {
-    this.cache[eventName].forEach(fn=>fn())
+
+  emit(eventName, ...args) {
+    if (!this.cache[eventName]) {
+      return console.log("can't emit an empty event")
+    }
+    this.cache[eventName].forEach(item=>item.fn.apply(item.ctx, args))
   }
+
   has(eventName) {
-    return this.cache[eventName] && !!this.cache[eventName].length
+    return !!this.cache[eventName] && !!this.cache[eventName].length
   }
+
   off(eventName, fn) {
-    const index = (this.cache[eventName] || []).find(item=>item === fn) || -1
+    const index = (this.cache[eventName] || []).find(item=>item.fn === fn) || -1
     if (index === -1) return
     this.cache[eventName].splice(index, 1)
   }
